10ee3f1eeca701ee0b615fad24ff84edca749e7b
권영란/GRM프로젝트 <youngran@gsretail.com>
2024-09-20 15:50:52 +0900

    feat: 새로운 기능 추가

--- src/main/java/com/example/spring_3_3_3/redis/controller/RedisController.java
+++ src/main/java/com/example/spring_3_3_3/redis/controller/RedisController.java
@@ -18,21 +18,21 @@ public class RedisController {
 
     @GetMapping("/redis/sample1")
     public void sample1() throws MalformedURLException {
-        String userId = "ran1";
+        String userId = "test1";
         URL url = new URL("http://localhost:8081");
         redisService.sample1(userId, url);
     }
 
     @GetMapping("/redis/sample2")
     public void sample2() throws MalformedURLException {
-        String userId = "ran2";
+        String userId = "test2";
         URL url = new URL("http://localhost:8082");
         redisService.sample2(userId, url);
     }
 
     @GetMapping("/redis/sample3")
     public void sample3() throws MalformedURLException {
-        String userId = "ran3";
+        String userId = "test3";
         URL url = new URL("http://localhost:8083");
         redisService.sample3(userId, url);
     }
